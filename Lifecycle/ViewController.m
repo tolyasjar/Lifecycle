91f37011bfaa1aa40f511016f1d1c0bc8b1f2ba3
tolyasjar <toleenjaradat@Toleens-MacBook-Pro.local>
2016-06-16 12:02:21 -0500

    Life Cycle Project

--- Lifecycle/ViewController.m
+++ Lifecycle/ViewController.m
@@ -15,13 +15,34 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"viewDidLoad");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)viewWillAppear:(BOOL)animated {
+    NSLog(@"viewWillAppear");
+    [super viewWillAppear:YES];
+}
 
+-(void)viewDidAppear {
+    [super viewDidAppear:YES];
+    NSLog(@"viewDidAppear");
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:YES];
+    NSLog(@"viewWillDisappear");
+    
+}
+-(void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:YES];
+    NSLog(@"viewDidDisappear");
+    
+}
 @end
